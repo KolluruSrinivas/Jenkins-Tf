2652bf270c5c3b0c0deabd634110a186748a403a
KolluruSrinivas <srinivasraokolluru@gmail.com>
2025-03-05 08:18:10 -0500

    second commit

---
+++ variables.tf
@@ -0,0 +1,12 @@
+variable "rgname" {
+    type = string
+    description = "used for naming resource group"
+    default = "rg2"
+  
+}
+
+variable "rglocation" {
+    type = string
+    description = "used for selecting the location"
+    default = "east US"
+}
